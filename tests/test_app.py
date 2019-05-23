f1596fe2892e6c790c4bc0282d3153332c70ae36
Adrian Teng <pumpadrian@gmail.com>
2019-05-22 22:55:22 -0700

    should fail

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,8 +2,8 @@ import unittest
 
 class TestApp(unittest.TestCase):
 
-    def test_should_pass(self):
-        self.assertTrue(True)
+    def test_should_fail(self):
+        self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main()
