ab2869a6dbe431966b22c91bee94b52d782bc2bb
Adrian Teng-Amnuay <adrian.tengamnuay@hulu.com>
2019-05-24 06:30:20 -0700

    Add a basic signatures api

--- app.py
+++ app.py
@@ -2,14 +2,35 @@ from gevent import monkey
 monkey.patch_all()
 
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 from gevent.pywsgi import WSGIServer
+
+from db import mongodb
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/api/signatures', methods=['GET', 'POST'])
+def signatures():
+    db = mongodb()
+    if request.method == 'POST':
+        body = request.get_json()
+        rsp = db['signatures'].insert_one({
+            'author': body.get('author'),
+            'message': body.get('message'),
+        })
+        return jsonify({
+            '_id': str(rsp.inserted_id)
+        })
+    else:
+        return jsonify([{
+            '_id': str(x['_id']),
+            'author': x['author'],
+            'message': x['message'],
+        } for x in db['signatures'].find({})])
 
 if __name__ == '__main__':
     server = WSGIServer(('0.0.0.0', int(os.environ.get('PORT', 8080))), app)
